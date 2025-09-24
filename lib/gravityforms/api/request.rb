4c26195ef4294987d4a841f005fc2c36aa93af46
Joe Slag <joe@slagwerks.com>
2025-09-24 11:31:28 -0500

    Add proxy option
    also fixed bug with offset option
    
    and reformatted, sorry

--- lib/gravityforms/api/request.rb
+++ lib/gravityforms/api/request.rb
@@ -7,32 +7,33 @@ module Gravityforms
   module Api
     class Request
       attr_reader :url
-      def initialize(route, method, per_page=20, offset=0, timeout=5)
-        expires = (Time.now+60*60).to_i
+      def initialize(route, method, per_page = 20, offset = 0, timeout = 5, proxy = nil)
+        expires = (Time.now + 60 * 60).to_i
         api_key = Gravityforms::Api.configuration.api_key
         api_url = Gravityforms::Api.configuration.api_url
         signature = calculate_signature(route, method, expires, api_key)
-        encode = "?api_key=#{api_key}&expires=#{expires}&signature=#{signature}&paging[page_size]=#{per_page}&paging[offset]=offset"
+        encode = "?api_key=#{api_key}&expires=#{expires}&signature=#{signature}&paging[page_size]=#{per_page}&paging[offset]=#{offset}"
         @url = "#{api_url}#{route}/#{encode}"
 
-        @connection = Faraday::Connection.new(nil, request: { timeout: timeout })
+        request_params = {timeout:}
+        request_params[proxy] = proxy if proxy
+
+        @connection = Faraday::Connection.new(nil, request: request_params)
       end
 
       def get
-        response = @connection.get(self.url)
+        @connection.get(url)
       end
 
       def post(payload)
-        response = @connection.post(self.url, payload)
+        @connection.post(url, payload)
       end
 
       def calculate_signature(route, method, expires, api_key)
         private_key = Gravityforms::Api.configuration.private_key
         string_to_sign = sprintf("%s:%s:%s:%s", api_key, method, route, expires)
-        hmac = OpenSSL::HMAC.digest('sha1',private_key,string_to_sign)
-        signature = CGI.escape(Base64.encode64("#{hmac}")).gsub("%0A", "")
-        #puts route + " : " + string_to_sign + " : " + signature
-        return signature
+        hmac = OpenSSL::HMAC.digest("sha1", private_key, string_to_sign)
+        CGI.escape(Base64.encode64(hmac.to_s)).gsub("%0A", "")
       end
     end
   end
